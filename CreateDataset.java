dd456a5034665aa2fb84aecd18a20cee6ad2b825
Subramanya Joshi <subramanya.joshi@cloudandheat.com>
2016-06-01 09:46:43 +0200

    adding simulator graphs

---
+++ CreateDataset.java
@@ -0,0 +1,93 @@
+package utils;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.Socket;
+import java.security.Timestamp;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAccessor;
+import java.time.temporal.TemporalField;
+import java.time.temporal.TemporalUnit;
+import java.time.temporal.ValueRange;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
+
+import javax.print.attribute.standard.DateTimeAtCompleted;
+
+import org.influxdb.InfluxDB;
+import org.influxdb.InfluxDB.ConsistencyLevel;
+import org.influxdb.InfluxDBFactory;
+import org.influxdb.dto.BatchPoints;
+import org.influxdb.dto.Point;
+import org.influxdb.dto.Query;
+
+public class CreateDataset {
+
+	static final String USER_ARRIVAL_LOG = "/Users/subramanya/Documents/workspace/autoscaling/logs/timeseries_citrix_rtc_minute.csv";
+	static final String CSV_DELIMITOR = ";";
+	static final String USER_ARRIVAL_DATA_SET = "/Users/subramanya/Documents/workspace/autoscaling/logs/rtc_70_created_dataset.csv";
+
+	public static void main(String[] args) {
+		BufferedReader br;
+		String line = "";
+		BufferedWriter bw;
+		int maxInstance = 6;
+		double percentOfRI = 0.7;
+		int countRI = (int) Math.ceil(maxInstance * percentOfRI);
+		int numberOfUserPerInstance = 120;
+		int userCoveredInRI = countRI * numberOfUserPerInstance;
+		InfluxDB influxDB = InfluxDBFactory.connect("http://80.156.222.17:8086", "root", "root");
+		String dbName = "aTimeSeries";
+		influxDB.createDatabase(dbName);
+
+		// Flush every 2000 Points, at least every 100ms
+		influxDB.enableBatch(2000, 100, TimeUnit.MILLISECONDS);
+		LocalDateTime lc = LocalDateTime.of(2015, 5, 28, 13, 0, 0);
+		System.out.println(lc.toEpochSecond(ZoneOffset.UTC));
+
+		// System.exit(0);
+
+		try {
+			br = new BufferedReader(new FileReader(CreateDataset.USER_ARRIVAL_LOG));
+			long start = 1432818000;
+			bw = new BufferedWriter(new FileWriter(CreateDataset.USER_ARRIVAL_DATA_SET));
+
+			while ((line = br.readLine()) != null) {
+				String[] dataRow = line.split(CSV_DELIMITOR);
+				int userReqCount = (int) Math.ceil(Double.parseDouble(dataRow[1]));
+				if (userReqCount < userCoveredInRI)
+					userReqCount = userCoveredInRI;
+				bw.write(start + "," + userReqCount);
+				bw.newLine();
+				Point point1 = Point.measurement("request").time(start, TimeUnit.SECONDS)
+						.field("arrival70", userReqCount).build();
+
+				influxDB.write(dbName, "default", point1);
+				System.out.println(start + "," + userReqCount);
+				start += 60;
+			}
+			bw.flush();
+			bw.close();
+			br.close();
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
--- r_scripts/cost_graphs.r
+++ r_scripts/cost_graphs.r
@@ -10,10 +10,23 @@ cost_header = c("baseline","ricost","odicost","totalcost","ricount","odicount");
 cost_log = read.csv(COST_LOG,sep=",",header=F);
 colnames(cost_log)  <- cost_header; 
 mat = as.matrix(cost_log)
-rownames(mat) = c("opt1","opt2","opt3","opt4","opt5","opt6","opt7")
+
+name = c()
+j = dim(mat)
+
+
+for(i in seq(1,j[1]))
+{
+	temp = paste("opt",as.character(i),sep="");
+	name = c(name,temp)
+}
+
+rownames(mat) = name
 tmat=t(mat)
 
-pdf("cost_graphs.pdf",width=20,height=10)
+wid = (j[1] * 10) / 7;
+
+pdf("cost_graphs.pdf",width=wid,height=10)
 opar <- par(no.readonly=TRUE)
 bp=barplot(tmat,main="Cost of each option to reserve instance",xlab="Options", ylab="Cost in USD",col=c(2:7),beside=T,legend=colnames(mat),horiz = F)
 text(bp, 0, round(tmat, 1),cex=1.5,pos=3,offset=19,srt = 90) 
